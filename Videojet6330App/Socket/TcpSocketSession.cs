1032a0986384c2fbe3115ab16d8338236904c854
viktor.volkov <volkov.vik1@gmail.com>
2020-10-02 10:10:33 +0300

    #UPD-3 Correct socket session message

--- Videojet6330App/Socket/TcpSocketSession.cs
+++ Videojet6330App/Socket/TcpSocketSession.cs
@@ -14,17 +14,14 @@ namespace Videojet6330App.Socket
         private bool _stop;
         private string _result;
         private Encoding _encoding;
-
-        private readonly int _port;
-        private readonly string _host;
         private readonly Logger _logger;
+        private readonly string _description;
 
         public TcpSocketSession(string host, int port, Logger logger): base(host, port)
         {
-            _host = host;
-            _port = port;
-            _logger = logger;
             _encoding = Encoding.UTF8;
+            _logger = logger;
+            _description = $"[Socket] TCP client {host}:{port}";
         }
 
         private readonly AsyncAutoResetEvent _connectEvent = new AsyncAutoResetEvent(false);
@@ -39,13 +36,13 @@ namespace Videojet6330App.Socket
             {
                 _stop = true;
                 if (!ConnectAsync())
-                    throw new InvalidOperationException("TCP client could not connect");
+                    throw new InvalidOperationException($"{_description} could not connect");
                 await _connectEvent.WaitAsync(cts.Token);
                 return !cts.IsCancellationRequested;
             }
             catch (OperationCanceledException)
             {
-                _logger.Error("[Socket] TCP client connect timeout");
+               _logger.Error($"{_description} connect timeout");
                 return false;
             }
         }
@@ -57,13 +54,13 @@ namespace Videojet6330App.Socket
             {
                 _stop = true;
                 if (!DisconnectAsync())
-                    throw new InvalidOperationException("TCP client could not disconnect");
+                    throw new InvalidOperationException($"{_description} could not disconnect");
                 await _disconnectEvent.WaitAsync(cts.Token);
                 return !cts.IsCancellationRequested;
             }
             catch (OperationCanceledException)
             {
-                _logger.Error("[Socket] TCP client disconnect timeout");
+                _logger.Error($"{_description} disconnect timeout");
                 return false;
             }
         }
@@ -74,13 +71,13 @@ namespace Videojet6330App.Socket
             try
             {
                 if (!SendAsync(data))
-                    throw new InvalidOperationException("TCP client could not send data");
+                    throw new InvalidOperationException($"{_description} could not send data");
                 await _sendEvent.WaitAsync(cts.Token);
                 return !cts.IsCancellationRequested;
             }
             catch (OperationCanceledException)
             {
-                _logger.Error("[Socket] TCP client send timeout");
+                _logger.Error($"{_description} send data timeout");
                 return false;
             }
         }
@@ -91,13 +88,13 @@ namespace Videojet6330App.Socket
             try
             {
                 if (!SendAsync(data))
-                    throw new InvalidOperationException("TCP client could not request execute");
+                    throw new InvalidOperationException($"{_description} could not request execute");
                 await _receivedEvent.WaitAsync(cts.Token);
                 return !cts.IsCancellationRequested ? _result : null;
             }
             catch (OperationCanceledException)
             {
-                _logger.Error("[Socket] TCP client request timeout");
+                _logger.Error($"{_description} request timeout");
                 return null;
             }
         }
@@ -109,13 +106,13 @@ namespace Videojet6330App.Socket
             try
             {
                 if (!SendAsync(data))
-                    throw new InvalidOperationException("TCP client could not request execute byte array");
+                    throw new InvalidOperationException($"{_description} could not request execute byte array");
                 await _receivedEvent.WaitAsync(cts.Token);
                 return !cts.IsCancellationRequested ? _result : null;
             }
             catch (OperationCanceledException)
             {
-                _logger.Error("[Socket] TCP client request byte array timeout");
+                _logger.Error($"{_description} request byte array timeout");
                 return null;
             }
         }
@@ -124,13 +121,13 @@ namespace Videojet6330App.Socket
         {
             _stop = false;
             _connectEvent.Set();
-            _logger?.Debug($"[Socket] TCP client {_host}:{_port} connected a new session with Id {Id}");
+            _logger?.Debug($"{_description} connected a new session with Id {Id}");
         }
 
         protected override void OnDisconnected()
         {
             _disconnectEvent.Set();
-            _logger?.Debug($"[Socket] TCP client {_host}:{_port} disconnected a session with Id {Id}");
+            _logger?.Debug($"{_description} disconnected a session with Id {Id}");
             if (_stop)
                 return;
             // Wait for a while...
@@ -143,17 +140,17 @@ namespace Videojet6330App.Socket
         {
             if (pending == 0)
                 _sendEvent.Set();
-            _logger?.Debug($"[Socket] TCP client {_host}:{_port} sent {sent} byte and pending {pending} byte");
+            _logger?.Debug($"{_description} sent {sent} byte and pending {pending} byte");
         }
 
         protected override void OnReceived(byte[] buffer, long offset, long size)
         {
             _result = _encoding.GetString(buffer, (int) offset, (int) size);
             _receivedEvent.Set();
-            _logger?.Debug($"[Socket] TCP client {_host}:{_port} receive {size} byte");
+            _logger?.Debug($"{_description} receive {size} byte");
         }
 
         protected override void OnError(SocketError error) =>
-            _logger?.Error($"[Socket] TCP client {_host}:{_port} caught an error with code {error}");
+            _logger?.Error($"{_description} caught an error with code {error}");
     }
 }
